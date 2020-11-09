8c2ce53bd688c68b3ed58f799782090f42e07e1b
xbjs <1098396951@qq.com>
2020-11-09 16:08:15 +0800

    '第二次提交'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "@/router";  //index可以省略
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+//1.安装插件
+Vue.use(VueRouter)
+
+//2.创建router对象
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Profile = () => import('../views/profile/Profile')
+const Shopcart = () => import('../views/shopcart/Shopcart')
+
+const routes = [
+  {
+    path: '',               //配置url和组件的映射关系
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+  {
+    path: '/shopcart',
+    component: Shopcart
+  },
+]
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+//3.导出router
+export default router
