e60042d45a039bdc1834f962e9b127de5a173e39
xbjs <1098396951@qq.com>
2020-11-11 14:40:47 +0800

    '第三次提交，完善首页轮播图和推荐信息'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,12 +2,20 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <home-feature-view></home-feature-view>
+    <ul>
+
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./chlidren/HomeSwiper";
+import RecommendView from "@/views/home/chlidren/RecommendView";
+import HomeFeatureView from "@/views/home/chlidren/HomeFeatureView";
+
 import {getHomeMultidata} from "@/network/home";
 
 export default {
@@ -15,6 +23,8 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
+    RecommendView,
+    HomeFeatureView
   },
   data() {
     return {
@@ -34,8 +44,16 @@ export default {
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: white;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
 </style>
